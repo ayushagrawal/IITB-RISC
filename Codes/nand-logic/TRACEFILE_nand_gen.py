4003c965eaf19a5fd49327ce27a03ea2c054cafe
Ashwin Wagh <waghashwin96@gmail.com>
2016-10-15 20:31:49 +0530

    Add files via upload

--- Codes/nand-logic/TRACEFILE_nand_gen.py
+++ Codes/nand-logic/TRACEFILE_nand_gen.py
@@ -7,17 +7,14 @@ for i in range(10000) :
 	k1 = randint(0,pow(2,16)) ;
 	k2 = randint(0,pow(2,16)) ;
 	
-	k3 = not('{:016b}'.format(k1) and '{:016b}'.format(k2));
+	k3 = pow(2,16) +(~(k1 & k2));
 	if k3 == 0:
 		z_flag = 1 ;
 	else:
 		z_flag = 0 ;
 
 	string = ""
-	string = '1' + '{:016b}'.format(k1) + '{:016b}'.format(k2) +'{:01b}'.format(z_flag) + '{:016b}'.format(k3) + "\n"
+	string = '{:016b}'.format(k1) + '{:016b}'.format(k2) +'{:01b}'.format(z_flag) + '{:016b}'.format(k3) + "\n"
 	f.write(string);	 
-	string = ""
-	string = '0' + '{:016b}'.format(k1) + '{:016b}'.format(k2) +'0' + '{:016b}'.format(k3) + "\n"
-	f.write(string);
-
+	
 	
--- Codes/nand-logic/nand_logic.vhd
+++ Codes/nand-logic/nand_logic.vhd
@@ -8,7 +8,6 @@ use std.standard.all;
 entity nand_logic is 
 	port ( ra, rb : in std_logic_vector(15 downto 0);
 	       rc : out std_logic_vector(15 downto 0);
-	       clock : in std_logic ;
 	       zero_flag : out std_logic 
 	     );
 end entity;
@@ -18,16 +17,11 @@ architecture form of nand_logic is
 begin
 	rc1 <= not(ra and rb);	
 	
-	process(clock)
+	process(rc1)
 	begin
 	
-	if (falling_edge(clock)) then
 	rc <= rc1 ;
-	if (rc1 = "0000000000000000") then
-		zero_flag <= '1';
-	else 
-		zero_flag <= '0';
-	end if;
-	end if;
+	zero_flag <= not(rc1(0) or rc1(1) or rc1(2) or rc1(3) or rc1(4) or rc1(5) or rc1(6) or rc1(7) or 	rc1(8) or rc1(9) or rc1(10) or rc1(11) or rc1(12) or rc1(13) or rc1(14) or rc1(15)) ;
+	
 end process;
 end form;
