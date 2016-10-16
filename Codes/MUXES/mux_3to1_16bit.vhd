ececb7c0c1ed15d297db4e45020e88f4d29fbf49
Ashwin Wagh <waghashwin96@gmail.com>
2016-10-16 14:58:24 +0530

    Update mux_3to1_16bit.vhd

--- Codes/MUXES/mux_3to1_16bit.vhd
+++ Codes/MUXES/mux_3to1_16bit.vhd
@@ -5,7 +5,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity mux_3to1_16bit is 
-	port( in1,in2,in3 : in std_logic_vector(15 downto 0); 
+	port( in_00,in_01,in_10 : in std_logic_vector(15 downto 0); 
 	      control_signals : in std_logic_vector(1 downto 0); 
 	      out1: out std_logic_vector(15 downto 0));
 end entity;
@@ -18,6 +18,6 @@ begin
 sel0 <= (others => control_signals(1)) ;
 sel1 <= (others => control_signals(0)) ;
 
-out1 <= ((not sel0) and (not sel1) and (in1)) or ((not sel0) and sel1 and in2) or (sel0 and (not sel1) and in3);
+out1 <= ((not sel0) and (not sel1) and (in_00)) or ((not sel0) and sel1 and in_01) or (sel0 and (not sel1) and in_10);
 
 end Behave;
