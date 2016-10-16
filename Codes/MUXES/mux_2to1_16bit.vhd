6ee18b12ef3a047cbef035de0c31897e11da89b7
Ayush Agrawal <ayushagrawal@users.noreply.github.com>
2016-10-16 14:50:31 +0530

    Variable name changes

--- Codes/MUXES/mux_2to1_16bit.vhd
+++ Codes/MUXES/mux_2to1_16bit.vhd
@@ -5,7 +5,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity mux_2to1_16bit is 
-	port( in1,in2 : in std_logic_vector(15 downto 0); 
+	port( in0,in1 : in std_logic_vector(15 downto 0); 
 	      control_signals : in std_logic; 
 	      out1: out std_logic_vector(15 downto 0));
 end entity;
@@ -17,6 +17,6 @@ begin
 
 sel0 <= (others => control_signals) ;
 
-out1 <= ((not sel0) and (in1)) or ((sel0) and (in2)) ;
+out1 <= ((not sel0) and (in0)) or ((sel0) and (in1)) ;
 
 end Behave;
