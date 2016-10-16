35b819390ce594326d3e636caeabd0acec3fff31
Ashwin Wagh <waghashwin96@gmail.com>
2016-10-16 14:45:48 +0530

    Update and_gate_3input.vhd

--- Codes/and_gate_3input.vhd
+++ Codes/and_gate_3input.vhd
@@ -6,7 +6,7 @@ use ieee.std_logic_1164.all;
 
 entity and_gate_3input is 
 	port( input : std_logic_vector(2 downto 0);
-		  output : std_logic ;
+		  output : std_logic) ;
 end entity;
 
 architecture Behave of and_gate_3input is
