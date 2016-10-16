8f1a84097f1e60fa1d0ec9db61e8f809ab73e497
Ayush Agrawal <ayushagrawal@users.noreply.github.com>
2016-10-16 15:27:56 +0530

    Name changes

--- Codes/MUXES/mux_4to1_3bit.vhd
+++ Codes/MUXES/mux_4to1_3bit.vhd
@@ -5,7 +5,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity mux_4to1_3bit is 
-	port( pri_en_input,in3_5,in6_8,in9_11 : in std_logic_vector(2 downto 0); 
+	port( in_00,in_11,in_01,in_10 : in std_logic_vector(2 downto 0); 
 	      control_signals : in std_logic_vector(1 downto 0); 
 	      out1: out std_logic_vector(2 downto 0));
 end entity;
@@ -18,6 +18,6 @@ begin
 sel0 <= (others => control_signals(1)) ;
 sel1 <= (others => control_signals(0)) ;
 
-out1 <= ((not sel0) and (not sel1) and pri_en_input) or (not sel0) and (not sel1) and (in1)) or ((not sel0) and sel1 and in6_8) or (sel0 and (not sel1) and in9_11) or (sel0 and sel1 and in3_5);
+out1 <= ((not sel0) and (not sel1) and in_00) or ((not sel0) and sel1 and in_01) or (sel0 and (not sel1) and in_10) or (sel0 and sel1 and in_11);
 
 end Behave;
