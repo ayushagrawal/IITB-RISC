e616b7c795d90a428dd12602ac46ece47bec523f
Ayush Agrawal <ayushagrawal@users.noreply.github.com>
2016-10-16 19:10:44 +0530

    Update datapathComponents.vhd

--- Codes/Datapath/datapathComponents.vhd
+++ Codes/Datapath/datapathComponents.vhd
@@ -1,1 +1,6 @@
---
+component subtractor_16bit is
+	port ( 	ra , rb : in std_logic_vector(15 downto 0);
+		rc : out std_logic_vector(15 downto 0);		
+		zero_flag : out std_logic 
+		);
+end component;
