cf041b0a1e5fa10aeb6bdf7c01a4fb5038ec74cd
Ashwin Wagh <waghashwin96@gmail.com>
2016-10-16 09:52:07 +0530

    Add files via upload

--- Codes/and_gate_3input.vhd
+++ Codes/and_gate_3input.vhd
@@ -5,19 +5,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity and_gate_3input is 
-	port( pri_en_input,in3_5,in6_8,in9_11 : in std_logic_vector(2 downto 0); 
-	      control_signals : in std_logic_vector(1 downto 0); 
-	      out1: out std_logic_vector(2 downto 0));
+	port( input : std_logic_vector(2 downto 0);
+		  output : std_logic ;
 end entity;
 
 architecture Behave of and_gate_3input is
-	signal sel0,sel1 : std_logic_vector(2 downto 0);
-
+	
 begin 
 
-sel0 <= (others => control_signals(1)) ;
-sel1 <= (others => control_signals(0)) ;
-
-out1 <= ((not sel0) and (not sel1) and pri_en_input) or (not sel0) and (not sel1) and (in1)) or ((not sel0) and sel1 and in6_8) or (sel0 and (not sel1) and in9_11) or (sel0 and sel1 and in3_5);
-
+output <=input(0) and input(1) and input(2);
 end Behave;
