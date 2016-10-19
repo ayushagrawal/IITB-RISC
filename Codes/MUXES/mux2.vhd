41f93b81faff74df86f98035a38ee4712f824faa
VikramBhosale <vkrmbhosale1@gmail.com>
2016-10-19 22:18:11 +0530

    Update mux2.vhd

--- Codes/MUXES/mux2.vhd
+++ Codes/MUXES/mux2.vhd
@@ -4,14 +4,14 @@ use std.standard.all;
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity mux is 
+entity mux2 is 
 	generic (n : integer);
 	port( in1,in2 : in std_logic_vector(n downto 0); 
 	      sel : in std_logic; 
 	      output : out std_logic_vector(n downto 0));
 end entity;
 
-architecture Behave of mux is
+architecture Behave of mux2 is
 	signal sel0 : std_logic_vector(n downto 0);
 
 begin 
